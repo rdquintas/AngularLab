ac590cbe94433e1c6a7ddddf858727e494ae8654
rdquintas <rqcenter-web@yahoo.com>
2017-11-06 17:55:03 +0000

    fixing mosh07

--- mosh07/src/app/zrqform/zrqform.component.ts
+++ mosh07/src/app/zrqform/zrqform.component.ts
@@ -7,12 +7,12 @@ import { Component, Input } from '@angular/core';
 })
 export class ZrqformComponent {
   @Input() name: string;
-  @Input() category: string[];
+  @Input() categories: string[];
   @Input() hasGuarantee: boolean;
 
   constructor() {
     this.name = 'nome do curso';
-    this.category = ['', 'Developement', 'Art', 'Languages'];
+    this.categories = ['', 'Developement', 'Art', 'Languages'];
     this.hasGuarantee = true;
   }
 
