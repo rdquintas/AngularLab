8a73948a7f3dadfb871026b1e885eab0e65588ea
rdquintas <rqcenter-web@yahoo.com>
2017-12-14 16:59:44 +0000

    adding link validation hide/show

--- mosh11/src/app/services/auth.service.ts
+++ mosh11/src/app/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
@@ -25,7 +26,22 @@ export class AuthService {
   }
 
   isLoggedIn() {
-    return false;
+
+    // this function belongs angular2-jwt library
+    // and it does exactly the same thing we coded below
+    return tokenNotExpired();
+
+    // const jwtHelper = new JwtHelper();
+    // const token = localStorage.getItem('token');
+
+    // if (!token) {
+    //   return false;
+    // }
+
+    // const expDate = jwtHelper.getTokenExpirationDate(token);
+    // const isExpired = jwtHelper.isTokenExpired(token);
+
+    // return !isExpired;
   }
 }
 
