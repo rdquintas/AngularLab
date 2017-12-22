a67b98cc6c2df51275ef5fe37e0b647e0caf2ff4
rdquintas <rqcenter-web@yahoo.com>
2017-12-22 18:23:56 +0000

    animations-demo

--- animations-demo/src/app/todos/todos.component.ts
+++ animations-demo/src/app/todos/todos.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
+  animations: [
+    trigger('fade', [
+      // state(),
+      transition('void => *', [
+        style({ backgroundColor: 'yellow', opacity: 0 }),
+        animate(2000, style({ backgroundColor: 'white', opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class TodosComponent {
   items: any[] = [
-    'Wash the dishes', 
-    'Call the accountant', 
+    'Wash the dishes',
+    'Call the accountant',
     'Apply for a car insurance'];
 
   addItem(input: HTMLInputElement) {
     this.items.splice(0, 0, input.value);
-    input.value = ''; 
+    input.value = '';
   }
 
   removeItem(item) {
