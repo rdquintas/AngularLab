ebc3aae672a7e7af225e6100a93a249f2bbd5c44
rdquintas <rqcenter-web@yahoo.com>
2018-01-10 15:41:40 +0000

    adding ZIPYY example

--- animations-demo/src/app/zippy/zippy.component.ts
+++ animations-demo/src/app/zippy/zippy.component.ts
@@ -1,15 +1,30 @@
 import { Component, Input } from '@angular/core';
+import { animation, animate, trigger, style, state, transition } from '@angular/animations';
 
 @Component({
   selector: 'zippy',
   templateUrl: './zippy.component.html',
   styleUrls: ['./zippy.component.css'],
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({ height: 0, overflow: 'hidden', paddingTop: 0, paddingBottom: 0 })),
+      state('expanded', style({ height: '*', overflow: 'auto', padding: '*' })),
+
+      transition('collapsed => expanded', [
+        animate('300ms ease-out')
+      ]),
+
+      transition('expanded => collapsed', [
+        animate('300ms ease-in')
+      ])
+    ])
+  ]
 })
-export class ZippyComponent  {
+export class ZippyComponent {
   @Input('title') title: string;
   isExpanded: boolean;
 
-  toggle() { 
+  toggle() {
     this.isExpanded = !this.isExpanded;
   }
 }
