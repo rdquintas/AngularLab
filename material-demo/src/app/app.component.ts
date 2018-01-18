d8b201ecac58251010ebfdf984cedcd0ed993e14
rdquintas <rqcenter-web@yahoo.com>
2018-01-18 16:06:42 +0000

    adding radio option group

--- material-demo/src/app/app.component.ts
+++ material-demo/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   isChecked = false;
+  isRadioButtonChecked = true;
 
   onChange(event) {
     console.log(event);
