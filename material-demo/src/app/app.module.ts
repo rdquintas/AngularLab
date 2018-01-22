57898a736d94825a4eec91bf7cb52335a023dc42
rdquintas <rqcenter-web@yahoo.com>
2018-01-22 15:52:31 +0000

    fixing app material-demo

--- material-demo/src/app/app.module.ts
+++ material-demo/src/app/app.module.ts
@@ -1,14 +1,32 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import {
+  MatCheckboxModule, MatRadioModule, MatSelectModule, MatInputModule, MatDatepickerModule,
+  MatNativeDateModule, MatIconModule, MatButtonModule, MatChipsModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatButtonModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
