ec996ff5e6a55ab8047021898ca8aa26e1d03bff
rdquintas <rqcenter-web@yahoo.com>
2018-01-24 15:50:08 +0000

    redux demo app

--- redux-demo/src/app/app.component.ts
+++ redux-demo/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { IAppState } from './store';
+import { NgRedux, select } from '@angular-redux/store';
 import { Component } from '@angular/core';
+import { INCREMENT } from './actions';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Redux App';
+  @select('counter') contador;
+  @select(['messaging', 'newMessages']) newMessages;
+  @select((s: IAppState) => s.messaging.newMessages) newMessagesCount;
+
+  constructor(private ngRedux: NgRedux<IAppState>) {
+    // Isto seria necessário se eu nao usasse o decorator @select() lá em cima na var counter
+    // var subscription = ngRedux.subscribe(() => {
+    //   var store = ngRedux.getState();
+    //   this.counter = store.counter;
+    // });
+  }
+
+  increment() {
+    // this.counter++;
+    this.ngRedux.dispatch({ type: INCREMENT });
+  }
+
 }
--- redux-demo/src/app/store.ts
+++ redux-demo/src/app/store.ts
@@ -1,10 +1,26 @@
-// tslint:disable-next-line:no-empty-interface
+import { IAppState } from './store';
+import { INCREMENT } from './actions';
+
 export interface IAppState {
-
+    counter: number;
+    messaging?: {
+        newMessages: number;
+    };
 }
 
-export function rootReducer(state, action) {
-    return state;
+// this is used to initialize our store with counter:0
+export const INITIAL_STATE: IAppState = {
+    counter: 0,
+    messaging: {
+        newMessages: 5
+    }
+};
+
+export function rootReducer(state: IAppState, action): IAppState {
+    switch (action.type) {
+        case INCREMENT:
+            return { counter: state.counter + 1 };
+        default:
+            return state;
+    }
 }
-
-
