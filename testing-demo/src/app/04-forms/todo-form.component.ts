b4b9c8dfc105b9b6a3314d41165458991686961f
rdquintas <rqcenter-web@yahoo.com>
2018-02-15 10:51:59 +0000

    adding FORM test cases

--- testing-demo/src/app/04-forms/todo-form.component.ts
+++ testing-demo/src/app/04-forms/todo-form.component.ts
@@ -1,12 +1,13 @@
 
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
-export class TodoFormComponent { 
-  form: FormGroup; 
+export class TodoFormComponent {
+  form: FormGroup;
 
   constructor(fb: FormBuilder) {
     this.form = fb.group({
       name: ['', Validators.required],
+      // name: [''],
       email: [''],
     });
   }
