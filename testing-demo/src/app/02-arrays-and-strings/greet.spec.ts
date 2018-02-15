d92b72d5b84b232926042de674734f86725957ea
rdquintas <rqcenter-web@yahoo.com>
2018-02-15 10:28:56 +0000

    adding test cases

--- testing-demo/src/app/02-arrays-and-strings/greet.spec.ts
+++ testing-demo/src/app/02-arrays-and-strings/greet.spec.ts
@@ -2,6 +2,7 @@ import { greet } from './greet';
 
 describe('greet', () => {
     it('should include the name in the message', () => {
-        expect(greet('mosh')).toContain('mosh');
+        expect(greet('ricardo')).toContain('ricardo');
+        // expect(greet('ricardo')).toBe('Welcome ricardo');
     })
 });
