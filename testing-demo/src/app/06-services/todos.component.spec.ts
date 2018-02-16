9c45d5e11b31b1ddc76b75aa9fd1e37f25750f7a
rdquintas <rqcenter-web@yahoo.com>
2018-02-16 11:21:45 +0000

    unit testing sucks

--- testing-demo/src/app/06-services/todos.component.spec.ts
+++ testing-demo/src/app/06-services/todos.component.spec.ts
@@ -2,6 +2,7 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -12,10 +13,11 @@ describe('TodosComponent', () => {
     component = new TodosComponent(service);
   });
 
-  it('it should set todos property with the items returned from the server', () => {
-    const todos = [1, 2, 3];
+
+  it('should set todos property with the items returned from the server', () => {
+    // const todos = [1, 2, 3];
     spyOn(service, 'getTodos').and.callFake(() => {
-      return Observable.from([todos]);
+      return Observable.from([1, 2, 3]);
     });
 
     component.ngOnInit();
@@ -23,5 +25,28 @@ describe('TodosComponent', () => {
     // expect(component.todos.length).toBe(todos);
     expect(component.todos.length).toBe(3);
     // expect(component.todos.length).toBeGreaterThan(0);
+  });
+
+
+  it('should call the server to save the changes when a new todo item is added', () => {
+    const spy = spyOn(service, 'add').and.callFake(t => {
+      return Observable.empty();
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+
+  it('should add the new todo returned from the server', () => {
+    const todo = { id: 1 };
+    const spy = spyOn(service, 'add').and.callFake(t => {
+      return Observable.from([todo]);
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
   });
 });
